e803e038af11ed42106114d8e0b8923f44339117
kamil1808 <122380315+kamil1808@users.noreply.github.com>
2023-03-12 17:54:14 +0100

    Dodany Data Provider

--- src/test/java/org/example/DriverAndWaits.java
+++ src/test/java/org/example/DriverAndWaits.java
@@ -3,7 +3,7 @@ package org.example;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
