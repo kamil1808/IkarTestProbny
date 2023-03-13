5ffc6eca33072c1ba9b26556cb7b000daee24684
kamil1808 <122380315+kamil1808@users.noreply.github.com>
2023-03-13 12:25:53 +0100

    Wersja działająca Dodano komentarze i kilka waitów

--- src/test/java/org/example/After.java
+++ src/test/java/org/example/After.java
@@ -2,21 +2,33 @@ package org.example;
 
 import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.Objects;
 
 public class After extends WebElements {
 
     public void logOut() {
         driver.findElement(By.xpath("//*[@id=\"m_header_topbar\"]/div/ul/button")).click();
         waitForPageFullyLoaded();
-
-        String currenturl = driver.getCurrentUrl();
-        System.out.println(currenturl);
-        //Assert.isTrue(currenturl.equals(WebSiteURL), "NIE POWROCONO NA POCZATKOWA STRONE");
+        waitForPageToBe(WebSiteURL);
+        Assert.isTrue(driver.getCurrentUrl().equals(WebSiteURL), "NIE POWROCONO NA POCZATKOWA STRONE");
+        System.out.println("POPRAWNIE WYLOGOWANO");
+    }
+    public static boolean isDriverClosed() {
+        try {
+            driver.getCurrentUrl();
+            return false;
+        } catch (Exception Ex) {
+            return true;
+        }
     }
     public void closeAndQuit() {
         driver.close();
         driver.quit();
+        Assert.isTrue(isDriverClosed(), "Driver is not closed and quit properly.");
+        System.out.println("ZAKONCZONO SESJE I ZAMKNIETO PRZEGLADARKE");
     }
 
 }
